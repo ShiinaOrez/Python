da53720b420a4e204c953c6a696752e1cd3e4dc3
ShiinaOrez <shiina_orez@qq.com>
2019-02-27 13:03:14 +0800

    [fixed]login and auto-check verify code

--- spoc/main.py
+++ spoc/main.py
@@ -1,8 +1,9 @@
 import requests
 from spoc_test import connect
+from pprint import pprint
 
-USERNAME = "..."
-PASSWORD = "..."
+USERNAME = "...."
+PASSWORD = "...."
 
 HEADER = {
     "Accept": "application/json, text/javascript, */*; q=0.01",
@@ -15,14 +16,27 @@ HEADER = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36",
 }
 
+LOGIN = [
+    "http://spoc.ccnu.edu.cn/userLoginController/checkLogin",
+    "http://spoc.ccnu.edu.cn/userLoginController/getUserProfile",
+    "http://spoc.ccnu.edu.cn/userInfo/getUserInfo",
+]
+
+def get_class(session, userId):
+    param = {
+        "userId": userId,
+    }
+    response = session.get("http://spoc.ccnu.edu.cn/api/friend/getInitList", params=param)
+    pprint(response.json())
+
 def login_once():
     code = ""
     session = requests.Session()
-    session.get("http://spoc.ccnu.edu.cn/starmoocHomepage", headers = HEADER)
     response = session.get("http://spoc.ccnu.edu.cn/userLoginController/getVerifCode")
-    print(response.headers)
+#    print(session.cookies)
     q = response.json().get("data")
     result = connect(q)
+#    print(result)
     for reg in result.get("Result").get("regions"):
         for line in reg.get("lines"):
             for w in line.get("words"):
@@ -32,17 +46,18 @@ def login_once():
                     break
     if len(code) < 4:
         print("[log]Length too Short!")
+
+
     payload = {
         "loginName": USERNAME,
-        "password": USERNAME,
+        "password": PASSWORD,
         "verifCode": code
     }
-    response = session.post(
-        "http://spoc.ccnu.edu.cn/userLoginController/userLogin",
-        data = payload)
-    response = session.get("http://spoc.ccnu.edu.cn/studentHomepage")
-    if "assignmentUserId" in response.text:
-        print("Y")
+    for url in LOGIN:
+        response = session.post(url, data=payload)
+    userInfo = response.json()
+#    pprint(userInfo)
+    get_class(session, userInfo.get("data").get("userInfoVO").get("id"))
 
 if __name__ == "__main__":
     login_once()
